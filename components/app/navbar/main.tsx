9763091b7132b3e3c8a6544054624bd08847108d
ibeginnernoob <adheilgupta@gmail.com>
2025-09-28 17:43:34 +0530

    navbar design - works for all screen sizes

--- components/app/navbar/main.tsx
+++ components/app/navbar/main.tsx
@@ -3,6 +3,7 @@
 import Searchbar from './searchbar';
 import { Button } from '@chakra-ui/react';
 import CartIcon from './cartIcon';
+import { RxHamburgerMenu } from 'react-icons/rx';
 import { signOut } from '@/auth/auth-client';
 import { useRouter } from 'next/navigation';
 import { User } from 'lucide-react';
@@ -29,83 +30,108 @@ export default function Navbar({ role }: Props) {
 	};
 
 	return (
-		<div className="bg-white dark:bg-black py-3 border-b-[0.5px] border-solid border-gray-200 shadow-lg flex flex-row justify-between items-center z-10 lg:px-14">
-			<div className="flex-1 flex flex-row items-center px-10">
-				<div className="w-full flex flex-row items-center gap-6">
-					<span className="text-2xl font-roboto font-bold bg-gradient-to-r from-purple-600 to-purple-300 bg-clip-text text-transparent">
-						MerchMania
-					</span>
+		<div className="py-2 border-b-[0.5px] border-solid border-gray-200 shadow-lg flex flex-row justify-between items-center z-10">
+			<div className="flex-1 flex flex-row items-center pl-6 pr-3 sm:px-6 lg:px-10">
+				<div className="w-full flex flex-row items-center justify-between sm:hidden">
+					<RxHamburgerMenu />
 					<Searchbar />
 				</div>
-			</div>
-			<div className="hidden flex-1 flex-row items-center lg:flex">
-				{role === 'CREATOR' && (
-					<div className="w-full flex flex-row items-center justify-end gap-8">
-						<button onClick={() => handleNavigation('/influencer/dashboard')}>Dashboard</button>
-						<button onClick={() => handleNavigation('/influencer/new-product')}>
-							Create New Product
-						</button>
-						<button
-							onClick={handleSignOut}
-							className="flex items-center justify-center w-8 h-8 bg-purple-100 hover:bg-purple-200 rounded-full cursor-pointer transition-colors duration-200"
+				<div className="w-full hidden flex-row items-center justify-between gap-4 sm:flex lg:hidden">
+					<div className="flex-1 flex flex-row items-center gap-4">
+						<RxHamburgerMenu />
+						<span className="text-xl font-roboto font-bold bg-gradient-to-r from-purple-600 to-purple-300 bg-clip-text text-transparent">
+							MerchMania
+						</span>
+						<Searchbar />
+					</div>
+					<div className="flex flex-row items-center gap-4 flex-shrink-0">
+						<Button
+							// onClick={() => handleNavigation('/signin')}
+							className="ml-4 text-xs font-semibold font-roboto text-white bg-purple-500 border-gray-600 px-4 rounded-md hover:opacity-80 active:opacity-60"
 						>
-							<User size={18} className="text-purple-600" />
-						</button>
+							Log In
+						</Button>
+						<Button
+							// onClick={() => handleNavigation('/signup')}
+							className="text-xs font-semibold font-roboto bg-black text-white border border-solid border-gray-600 px-4 rounded-md hover:opacity-80 active:opacity-60"
+						>
+							Sign Up
+						</Button>
 					</div>
-				)}
-				{role === 'CUSTOMER' && (
-					<>
-						<div className="w-full flex flex-row items-center justify-end gap-8">
-							<button className="text-sm font-roboto text-gray-600 hover:text-purple-500 duration-200">
-								Explore
-							</button>
-							<button className="text-sm font-roboto text-gray-600 hover:text-purple-500 duration-200">
-								Our Influencers
-							</button>
+				</div>
+				<div className="w-full hidden flex-row items-center gap-4 lg:flex">
+					<div className="flex-auto flex flex-row items-center gap-4">
+						<span className="text-2xl font-roboto font-bold bg-gradient-to-r from-purple-600 to-purple-300 bg-clip-text text-transparent">
+							MerchMania
+						</span>
+						<Searchbar />
+					</div>
+					<div className="flex flex-row items-center gap-10">
+						<div className="flex flex-row items-center gap-4 flex-shrink-0">
+							{role !== 'CREATOR' ? (
+								<>
+									<button className="text-sm font-roboto text-gray-600 hover:text-purple-500 duration-200">
+										Explore
+									</button>
+									<button className="text-sm font-roboto text-gray-600 hover:text-purple-500 duration-200">
+										Our Influencers
+									</button>
+								</>
+							) : (
+								<>
+									<button onClick={() => handleNavigation('/influencer/dashboard')}>
+										Dashboard
+									</button>
+									<button onClick={() => handleNavigation('/influencer/new-product')}>
+										Create New Product
+									</button>
+								</>
+							)}
 						</div>
-						<div className="flex flex-row items-center gap-6">
-							<button>Cart</button>
-							<button
-								onClick={handleSignOut}
-								className="flex items-center justify-center w-8 h-8 bg-purple-100 hover:bg-purple-200 rounded-full cursor-pointer transition-colors duration-200"
-							>
-								<User size={18} className="text-purple-600" />
-							</button>
-						</div>
-					</>
-				)}
-				{role === 'STRANGER' && (
-					<div className="w-full flex flex-row items-center justify-end gap-8">
-						<div className="w-fit flex flex-row justify-between gap-16">
-							<button className="text-sm font-roboto text-gray-600 hover:text-purple-500 duration-200">
-								Explore
-							</button>
-							<button className="text-sm font-roboto text-gray-600 hover:text-purple-500 duration-200">
-								Our Influencers
-							</button>
-						</div>
-						<div className="flex flex-row items-center gap-6">
-							<Button
-								// onClick={() => handleNavigation('/signin')}
-								onClick={() => handleNavigation('/signin')}
-								className="ml-4 text-xs font-semibold font-roboto text-white bg-purple-500 border-gray-600 px-4 rounded-md hover:opacity-80 active:opacity-60"
-							>
-								Log In
-							</Button>
-							<Button
-								onClick={() => handleNavigation('/signup')}
-								className="text-xs font-semibold font-roboto bg-black text-white border border-solid border-gray-600 px-4 rounded-md hover:opacity-80 active:opacity-60"
-							>
-								Sign Up
-							</Button>
+						<div className="flex flex-row items-center gap-4 flex-shrink-0">
+							{role === 'CREATOR' && (
+								<>
+									<button
+										onClick={handleSignOut}
+										className="flex items-center justify-center w-8 h-8 bg-purple-100 hover:bg-purple-200 rounded-full cursor-pointer transition-colors duration-200"
+									>
+										<User size={18} className="text-purple-600" />
+									</button>
+								</>
+							)}
+							{role === 'CUSTOMER' && (
+								<>
+									<button>Cart</button>
+									{/* <CartIcon /> */}
+									<button
+										onClick={handleSignOut}
+										className="flex items-center justify-center w-8 h-8 bg-purple-100 hover:bg-purple-200 rounded-full cursor-pointer transition-colors duration-200"
+									>
+										<User size={18} className="text-purple-600" />
+									</button>
+								</>
+							)}
+							{role === 'ANONYMOUS' ||
+								(role !== 'CREATOR' && role !== 'CUSTOMER' && (
+									<>
+										<Button
+											// onClick={() => handleNavigation('/signin')}
+											className="ml-4 text-xs font-semibold font-roboto text-white bg-purple-500 border-gray-600 px-4 rounded-md hover:opacity-80 active:opacity-60"
+										>
+											Log In
+										</Button>
+										<Button
+											// onClick={() => handleNavigation('/signup')}
+											className="text-xs font-semibold font-roboto bg-black text-white border border-solid border-gray-600 px-4 rounded-md hover:opacity-80 active:opacity-60"
+										>
+											Sign Up
+										</Button>
+									</>
+								))}
 						</div>
 					</div>
-				)}
+				</div>
 			</div>
 		</div>
 	);
-}
-
-{
-	/* <CartIcon /> */
 }
--- components/app/navbar/searchbar.tsx
+++ components/app/navbar/searchbar.tsx
@@ -3,15 +3,15 @@ import { CiSearch } from 'react-icons/ci';
 
 export default function Searchbar() {
 	return (
-		<div className="flex-1 max-w-[30rem]">
+		<div className="flex-1 max-w-[16rem] sm:max-w-[20rem]">
 			<InputGroup
 				className="flex flex-row items-center w-full"
 				startElement={
-					<CiSearch className="ml-3 cursor-pointer text-black dark:text-white" size={18} />
+					<CiSearch className="ml-3 cursor-pointer text-black dark:text-white" size={16} />
 				}
 			>
 				<Input
-					className="h-fit py-2 w-full px-5 border-[0.5px] border-gray-700 border-opacity-20 font-roboto text-xs text-gray-700 dark:text-white dark:placeholder:text-gray-300"
+					className="h-fit py-2 w-full px-4 border-[0.5px] border-gray-700 rounded-2xl border-opacity-20 font-roboto text-[12px] text-gray-700 dark:text-white dark:placeholder:text-gray-300"
 					placeholder="Search products, brands, or influencers..."
 				/>
 			</InputGroup>
