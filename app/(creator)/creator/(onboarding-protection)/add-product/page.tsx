460c72811f5dc3ce37107faea089d88954007b18
ibeginnernoob <adheilgupta@gmail.com>
2026-01-06 00:31:25 +0530

    refactor creator routes

--- app/(creator)/creator/(onboarding-protection)/add-product/page.tsx
+++ app/(creator)/creator/(onboarding-protection)/add-product/page.tsx
@@ -1,25 +1,11 @@
-import { auth } from '@/auth/auth';
-import { headers } from 'next/headers';
-import { redirect } from 'next/navigation';
-import Navbar from '@/components/app/navbar/Main';
-import Form from '@/components/app/pages/influ-create-product/Form';
-import Footer from '@/components/Footer';
-import { Roles } from '@/types';
+import { getSession } from "@/lib/auth";
+
+import Form from './components/form';
 
 async function Page() {
-  const session = await auth.api.getSession({
-    headers: await headers()
-  })
-  if (!session?.user || session.user.role !== 'creator' || !session.user.isOnboarded) {
-    redirect('/');
-  }
-
+  const session = await getSession();
   return (
-    <div className="flex flex-col">
-      <Navbar role={session?.user?.role as Roles || 'anonymous'} />
-      <Form userId={session?.user?.id} />
-      <Footer />
-    </div>
+    <Form userId={session?.user?.id || ''} />
   );
 };
 
